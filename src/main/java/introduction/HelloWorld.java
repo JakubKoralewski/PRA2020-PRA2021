2ab5f1a05d421e3c7f5a153ebc6c46f82aa61f2b
Jakub Koralewski <jakkor8@st.amu.edu.pl>
2020-10-19 18:29:58 +0200

    test

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,11 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+    static Logger log = Logger.getLogger("myLogger");
 
     public static void main(String [ ] args) {
-        System.out.println("Hello World");
+        log.info("Hello");
     }
 }
